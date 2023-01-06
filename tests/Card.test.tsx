c6504727467210b9040ccb03bf742ea7d4558ad4
IvanGodinez21 <ivano.godinez@gmail.com>
2023-01-05 23:23:28 -0600

    Create Card.test.tsx
    Notes:
    - To test Card (component)

---
+++ tests/Card.test.tsx
@@ -0,0 +1,48 @@
+// Node modules
+import React from 'react';
+import { describe, expect, it } from '@jest/globals';
+import { act, create } from 'react-test-renderer';
+// Classes
+import Property from '../src/classes/Property';
+// Components
+import Card from '../src/components/Card';
+// Database
+import properties from '../src/databases/properties.json';
+//
+
+describe('<Card />', () => {
+	let parsedProperties: Property[];
+	beforeAll(() => {
+		parsedProperties = properties as Property[];
+	});
+	it('can render all the properties from the database', () => {
+		jest.useFakeTimers();
+		const components = [];
+		for (const item of parsedProperties) {
+			const component = create(<Card item={item} />).toJSON();
+			if (component) {
+				components.push(component);
+			}
+		}
+		expect(components.length).toBe(properties.length);
+	});
+	it('has two childs', () => {
+		const component = create(<Card item={parsedProperties[0]} />).toJSON();
+		if (component && !(component instanceof Array)) {
+			expect(component.children?.length).toBe(2);
+		}
+	});
+	it('has an item property stored inside the props property ', () => {
+		const component = create(<Card item={parsedProperties[0]} />).getInstance();
+		if (component && !(component instanceof Array)) {
+			expect(component.props.item).toEqual(expect.any(Object));
+		}
+	});
+	test('button favorite can be pressed', () => {
+		const component = create(<Card item={parsedProperties[0]} />);
+		const favoriteButton = component.root.findByProps({ testID: 'favoriteButton' });
+		expect(component.root.props.item.favorite).toBe(false || null);
+		act(() => favoriteButton.props.onPress());
+		expect(component.root.props.item.favorite).toBe(true);
+	});
+});
